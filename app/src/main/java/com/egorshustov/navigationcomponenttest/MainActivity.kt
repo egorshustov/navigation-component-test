21f0069db97ede8e1db50e627f41bf67e512b083
Egor Shustov <egorshustov.93@gmail.com>
2019-07-10 23:31:05 +0500

    Fragments are created, layouts are completed, nav_graph is created

---
+++ app/src/main/java/com/egorshustov/navigationcomponenttest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.egorshustov.navigationcomponenttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
